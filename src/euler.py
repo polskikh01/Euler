3f9e1b0b2a05761a0fdd30a094820ce8d6a85918
polskikh01 <polskikh01@gmail.com>
2021-03-25 14:55:15 +0400

    Реализовано: фунцкия Эйлера 1 и 2 порядка

---
+++ src/euler.py
@@ -0,0 +1,19 @@
+import math
+
+
+def dxdy(x, y):
+    f = 2 * y + math.exp(x) - x
+    return f
+
+
+a = 0.0
+b = 1.0
+h = 0.1
+alphe = 0.25
+
+while (b - a) > h / 2:
+    z = alphe + (h * dxdy(a, alphe))
+    alphe = z
+    a += h
+    print("f(" + str(a) + ")=" + str(z))
+
---
+++ src/euler2.py
@@ -0,0 +1,18 @@
+def dxdy(x, y, dy):
+    f = dy + 2 * x - 2
+    return f
+
+
+h = 0.1
+
+x = [0]
+y = [1]
+dy = [1]
+n = 0
+
+for i in range(11):
+    x.append(x[i] + h)
+    y.append(y[i] + h * dy[i])
+    dy.append(dy[i] + h * dxdy(x[i], y[i], dy[i]))
+    print(x[i], y[i], dy[i])
+
